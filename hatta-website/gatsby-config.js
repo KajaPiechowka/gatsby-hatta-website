c3ae611d672f60143e63f9237266227c9a53d64e
Kaja Piechówka <kaja.piechowka@gmail.com>
2020-10-15 22:58:33 +0200

    gallery

--- hatta-website/gatsby-config.js
+++ hatta-website/gatsby-config.js
@@ -36,6 +36,13 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `gallery`,
+        path: `${__dirname}/src/assets/images/gallery`,
+      },
+    },
+    {
       resolve: `gatsby-source-datocms`,
       options: {
         apiToken: process.env.API_DATO_TOKEN,
@@ -44,20 +51,5 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-mdx`,
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `gatsby-starter-default`,
-    //     short_name: `starter`,
-    //     start_url: `/`,
-    //     background_color: `#663399`,
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-    //   },
-    // },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
--- hatta-website/src/pages/gallery.js
+++ hatta-website/src/pages/gallery.js
@@ -1,11 +1,69 @@
 import React from "react"
-import Post from "../components/Post/Post"
+import PageInfo from "../components/PageInfo/PageInfo"
+import styled from "styled-components"
+import { graphql } from "gatsby"
+import Image from "gatsby-image"
 
-const GalleryPage = () => (
-  <>
-    <Post></Post>
-    <h1>articles</h1>
-  </>
-)
+const galleryInfo = {
+  title: "Gallery",
+  paragraph:
+    "While artists work from real to the abstract, architects must work from the abstract to the real. ",
+}
+
+const StyledGallery = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin: 50px 0;
+`
+const StyledImage = styled(Image)`
+  min-width: 400px;
+  height: auto;
+  object-fit: cover;
+  margin: 10px;
+`
+
+const PhotoWrappper = styled.div`
+  width: 100vh;
+  height: 100vh;
+  display: flex;
+  justify-constent: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.4);
+`
+
+const SinglePhoto = styled(Image)`
+  width: 80%;
+  height: auto;
+  object-fit: cover;
+`
+
+const GalleryPage = ({ data }) => {
+  console.log(data)
+  return (
+    <>
+      <PageInfo title={galleryInfo.title} paragraph={galleryInfo.paragraph} />
+      <StyledGallery>
+        {data.allFile.nodes.map(item => (
+          <StyledImage key={item.id} fluid={item.childImageSharp.fluid} />
+        ))}
+      </StyledGallery>
+    </>
+  )
+}
+
+export const query = graphql`
+  {
+    allFile(filter: { relativeDirectory: { eq: "gallery" } }) {
+      nodes {
+        id
+        childImageSharp {
+          fluid(maxWidth: 1800) {
+            ...GatsbyImageSharpFluid_tracedSVG
+          }
+        }
+      }
+    }
+  }
+`
 
 export default GalleryPage
